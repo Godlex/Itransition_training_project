c9c84d1b001073301c5605554ba58c75bb273fe0
Alexei <alexei.truhan@gmail.com>
2021-12-09 14:55:17 +0300

    rewrite reduxer

--- quality_of_life_changer.client/src/redux-modules/auth/reducer.js
+++ quality_of_life_changer.client/src/redux-modules/auth/reducer.js
@@ -1,33 +1,34 @@
-import { authConstants } from "./constants"
+import { authConstants } from "./constants";
 
-const initialAuthState = {name:null,password:null};
-const initialRegisterState = {name:null,password:null,confirmPassword:null};
+const initialAuthState = {
+  login: { name: null, password: null },
+  register: { name: null, password: null, confirmPassword: null },
+};
 
-export function authReducer(state = initialAuthState, action) {
-    switch (action.type) {
-        case authConstants.LOGIN_SUCCESS:
-            return{
-                name: action.username,
-                password: action.password
-            }
-        case authConstants.LOGIN_FAILURE:
-            return {...initialAuthState};
-      default:
-        return state
-    }
-}
-
-export function registerReducer(state = initialRegisterState,action) {
-    switch (action.type) {
-        case authConstants.REGISTER_SECCESS:
-            return{
-                name: action.username,
-                password: action.password,
-                confirmPassword: action.confirmPassword
-            }
-        case authConstants.REGISTER_FAILURE:
-            return {...initialRegisterState};
-      default:
-        return state
-    }
+export default function authReducer(state = initialAuthState, action) {
+  switch (action.type) {
+    case authConstants.LOGIN_SUCCESS:
+      return {
+        login: {
+          name: action.username,
+          password: action.password,
+        },
+        register: initialAuthState.register
+      };
+    case authConstants.REGISTER_SECCESS:
+      return {
+        register: {
+          name: action.username,
+          password: action.password,
+          confirmPassword: action.confirmPassword
+        },
+        login: initialAuthState.login    
+      };
+    case authConstants.LOGIN_FAILURE:
+      return { ...initialAuthState };
+      case authConstants.REGISTER_FAILURE:
+      return { ...initialAuthState };
+    default:
+      return state;
+  }
 }
--- quality_of_life_changer.client/src/redux-modules/index-reducer.js
+++ quality_of_life_changer.client/src/redux-modules/index-reducer.js
@@ -1,9 +1,8 @@
 import { combineReducers } from "redux";
-import { registerReducer, authReducer } from "./auth/reducer";
+import authReducer from "./auth/reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  register: registerReducer,
 });
 
 export default rootReducer;
