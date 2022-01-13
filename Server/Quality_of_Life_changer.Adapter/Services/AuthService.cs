c4518e30e1c1718da891b09b5e0c4df154f4b95a
Alexei <alexei.truhan@gmail.com>
2022-01-13 16:09:22 +0300

    moving services from .WebApi to .Adapter

---
+++ Server/Quality_of_Life_changer.Adapter/Services/AuthService.cs
@@ -0,0 +1,57 @@
+﻿namespace Quality_of_Life_changer.Adapter.Services;
+
+using Contracts.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using Model.Auth;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Web.Helpers;
+
+public class AuthService : IAuthService
+
+{
+    private readonly int _jwtLifespan;
+    private readonly string _jwtSecret;
+
+
+    public AuthService(string jwtSecret, int jwtLifespan)
+    {
+        _jwtSecret = jwtSecret;
+        _jwtLifespan = jwtLifespan;
+    }
+
+    public bool VerifyPassword(string actualPassword, string hashedPassword)
+    {
+        return Crypto.VerifyHashedPassword(hashedPassword, actualPassword);
+    }
+
+    public AuthData GetAuthData(string id, string name, string email)
+    {
+        var expirationTime = DateTime.UtcNow.AddSeconds(_jwtLifespan);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, name),
+                new Claim(ClaimTypes.Email, email),
+                new Claim(ClaimTypes.NameIdentifier, id)
+            }),
+            Expires = expirationTime,
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSecret)),
+                SecurityAlgorithms.HmacSha256Signature
+            )
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+
+        return new AuthData
+        {
+            Token = token,
+            TokenExpirationTime = ((DateTimeOffset) expirationTime).ToUnixTimeSeconds(),
+            Id = id
+        };
+    }
+}
--- Server/Quality_of_Life_changer.WebApi/Program.cs
+++ Server/Quality_of_Life_changer.WebApi/Program.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json.Linq;
 using Quality_of_Life_changer.Adapter;
+using Quality_of_Life_changer.Adapter.Services;
 using Quality_of_Life_changer.Contracts.Commands;
 using Quality_of_Life_changer.Contracts.Interfaces;
 using Quality_of_Life_changer.Contracts.Queries;
 using Quality_of_Life_changer.Data;
 using Quality_of_Life_changer.Implication.Handlers.QueriesHandlers;
 using Quality_of_Life_changer.WebApi;
-using Quality_of_Life_changer.WebApi.Services;
 using Serilog;
 using System.Text;
 
