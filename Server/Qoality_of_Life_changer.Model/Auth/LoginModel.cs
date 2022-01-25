cc68527e56378311ae27135d29b3c4e255d80f13
Alexei <alexei.truhan@gmail.com>
2022-01-25 09:11:19 +0300

    fix code

--- Server/Qoality_of_Life_changer.Model/Auth/LoginModel.cs
+++ Server/Qoality_of_Life_changer.Model/Auth/LoginModel.cs
@@ -1,10 +1,8 @@
 ﻿namespace Quality_of_Life_changer.Model.Auth;
 
-using System.ComponentModel.DataAnnotations;
-
 public class LoginModel
 {
-    [Required] public string Email { get; set; }
+    public string Email { get; set; }
 
-    [Required] public string Password { get; set; }
+    public string Password { get; set; }
 }
